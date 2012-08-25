2d1cb4b478563b16f096aa51729085ea606fa807
Kaushik Chaubal <kaushikchaubal@gmail.com>
2012-08-25 15:36:37 +0100

    Adding UDP server-client prototype

---
+++ src/udp/connection/string/UdpClientString.java
@@ -0,0 +1,29 @@
+package udp.connection.string;
+
+import java.io.*;
+import java.net.*;
+
+public class UdpClientString {
+	public static void main(String args[]) throws Exception {
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
+				System.in));
+		DatagramSocket clientSocket = new DatagramSocket(4000);
+
+		byte[] address = new byte[] { (byte) 127, (byte) 0, (byte) 0, (byte) 1 };
+		InetAddress IPAddress = InetAddress.getByAddress(address);
+
+		byte[] receiveData = new byte[1024];
+
+		DatagramPacket receivePacket = new DatagramPacket(receiveData,
+				receiveData.length);
+
+		clientSocket.connect(IPAddress, 4000);
+		System.out.println("Listening for port 4000");
+		clientSocket.receive(receivePacket);
+		System.out.println("Received data");
+
+		String modifiedSentence = new String(receivePacket.getData());
+		System.out.println("FROM SERVER:" + modifiedSentence);
+		clientSocket.close();
+	}
+}
