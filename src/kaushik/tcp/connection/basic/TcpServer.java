d4e8c1b13048a050bf57c689da207bd6901a9634
Kaushik Chaubal <kaushikchaubal@gmail.com>
2012-08-25 14:19:52 +0100

    Adding basic TCP connection code

---
+++ src/kaushik/tcp/connection/basic/TcpServer.java
@@ -0,0 +1,27 @@
+package kaushik.tcp.connection.basic;
+
+import java.io.*;
+import java.net.*;
+
+public class TcpServer {
+	public static void main(String[] args) {
+		String clientSentence;
+		ServerSocket ss;
+		try {
+			ss = new ServerSocket(5000);
+			while (true) {
+				System.out.println("Waiting");
+				Socket serverSocket = ss.accept();
+
+				System.out.println("Connected");
+				DataInputStream inFromClient = new DataInputStream(
+						serverSocket.getInputStream());
+				clientSentence = inFromClient.readUTF();
+
+				System.out.println("Received: " + clientSentence);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
